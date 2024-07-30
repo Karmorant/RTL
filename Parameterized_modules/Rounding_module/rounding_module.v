4acf1eba68e7969a4393f6a8331e4bc58eadd803
Vladislav <diz28600@gmail.com>
2024-07-30 14:45:51 +0300

    added defines

--- Parameterized_modules/Rounding_module/rounding_module.v
+++ Parameterized_modules/Rounding_module/rounding_module.v
@@ -1,3 +1,6 @@
+`define ROUNDING_SIFT 32
+`define MANT_START 10
+
 module rounding_module
 #(
         parameter DATA_W = 30
@@ -9,16 +12,16 @@ module rounding_module
 );
 
 
-wire rounding_bit = data[DATA_W-1-31];
-wire sticky_bit   = (DATA_W > 32) ? | data[DATA_W-1-1-31 : 0] : 0;
+wire rounding_bit = data[DATA_W - `ROUNDING_SIFT];
+wire sticky_bit   = (DATA_W > `ROUNDING_SIFT) ? | data[DATA_W-1-`ROUNDING_SIFT : 0] : 0;
 
-assign mant =   (rouding_mode == 1                               ) ? data[DATA_W-1-9 : DATA_W-1-31]        :  // rounding to 0
-                (rouding_mode == 2 && !data[DATA_W - 1]          ) ? data[DATA_W-1-9 : DATA_W-1-31] + 1'b1 :  // rounding to +INF(+)
-                (rouding_mode == 2 &&  data[DATA_W - 1]          ) ? data[DATA_W-1-9 : DATA_W-1-31]        :  // rounding to +INF(-)
-                (rouding_mode == 3 &&  data[DATA_W - 1]          ) ? data[DATA_W-1-9 : DATA_W-1-31] + 1'b1 :  // rounding to -INF(+)
-                (rouding_mode == 3 && !data[DATA_W - 1]          ) ? data[DATA_W-1-9 : DATA_W-1-31]        :  // rounding to -INF(-)
-                (rouding_mode == 0 &&  rounding_bit && sticky_bit) ? data[DATA_W-1-9 : DATA_W-1-31] + 1'b1 :  // rounding to nearest even
-                                                                     data[DATA_W-1-9 : DATA_W-1-31];
+assign mant =   (rouding_mode == 1                               ) ? data[DATA_W-`MANT_START : DATA_W-`ROUNDING_SIFT]        :  // rounding to 0
+                (rouding_mode == 2 && !data[DATA_W - 1]          ) ? data[DATA_W-`MANT_START : DATA_W-`ROUNDING_SIFT] + 1'b1 :  // rounding to +INF(+)
+                (rouding_mode == 2 &&  data[DATA_W - 1]          ) ? data[DATA_W-`MANT_START : DATA_W-`ROUNDING_SIFT]        :  // rounding to +INF(-)
+                (rouding_mode == 3 &&  data[DATA_W - 1]          ) ? data[DATA_W-`MANT_START : DATA_W-`ROUNDING_SIFT] + 1'b1 :  // rounding to -INF(+)
+                (rouding_mode == 3 && !data[DATA_W - 1]          ) ? data[DATA_W-`MANT_START : DATA_W-`ROUNDING_SIFT]        :  // rounding to -INF(-)
+                (rouding_mode == 0 &&  rounding_bit && sticky_bit) ? data[DATA_W-`MANT_START : DATA_W-`ROUNDING_SIFT] + 1'b1 :  // rounding to nearest even
+                                                                     data[DATA_W-`MANT_START : DATA_W-`ROUNDING_SIFT];
 
 
 endmodule
